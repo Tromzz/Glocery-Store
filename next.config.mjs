25d64a27ff1c249cec67a05a2bc6c342e6c657f8
Tromzz <midhudhanya10@gmail.com>
2025-07-29 15:13:55 +0530

    Add configuration files and update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: 'export',
-    trailingSlash: true,
     images: {
-        unoptimized: true,
         domains:['127.0.0.1']
     }
 };
